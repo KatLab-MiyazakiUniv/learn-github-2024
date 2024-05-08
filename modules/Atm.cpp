33cd6a2059e74a4bd2eb9aa22ed8636c6fa1a9d7
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-05-08 10:07:50 +0000

    update: Atmのテストをみんなで実装！！！！！

--- modules/Atm.cpp
+++ modules/Atm.cpp
@@ -1,22 +1,40 @@
 #include "Atm.h"
 
 // コンストラクタ
-Atm::Atm(int init) : balance(init) {}
+Atm::Atm(int init) {
+    if(init < 0){
+        balance = 0;
+    }else{
+        balance = init;
+    }
+}
 
 // 残高(balance)を返す
 int Atm::getBalance()
 {
+    // if (balance > 0){
     return balance;
+    // }else{
+    //     return 0;
+    // }
 }
 
 // 入金
+//ATM.cpp
 void Atm::deposit(int amount)
 {
+    if(amount < 0 ){
+        return;
+    }
+    
     balance += amount;
 }
 
 // 引き出し
 void Atm::withdrawal(int amount)
 {
-    balance -= amount;
+    if (amount > 0 && balance - amount >= 0)
+    {
+        balance -= amount;
+    }
 }
--- test/AtmTest.cpp
+++ test/AtmTest.cpp
@@ -23,5 +23,80 @@ namespace etrobocon2024_learn
         EXPECT_EQ(expected, atm.getBalance());
     }
 
+    TEST(Atm, AtmBalancedeposit)
+    {
+        Atm atm(1000);
+
+        atm.deposit(1000);
+
+        int expected = 2000;
+
+        EXPECT_EQ(expected, atm.getBalance());
+    }
+
+    TEST(Atm, AtmBalancewithdrawal)
+    {
+        Atm atm(1000);
+
+        atm.withdrawal(1000);
+
+        int expected = 0;
+
+        EXPECT_EQ(expected, atm.getBalance());
+    }
+
+    TEST(Atm, ATMDeposit){
+
+        Atm atm(0);
+
+        atm.deposit(1000);
+
+        int expected = 1000;
+
+        EXPECT_EQ(expected, atm.getBalance());
+    }
+
+    TEST(Atm, ATMDepositMinus){
+
+        Atm atm(1000);
+
+        atm.deposit(-1000);
+
+        int expected = 1000;
+
+        EXPECT_EQ(expected, atm.getBalance());
+    }
+
+    TEST(Atm, AtmWithdrawCorrect)
+    {
+        Atm atm(1000);
+        atm.withdrawal(1000);
+        int expected = 0;
+        EXPECT_EQ(expected, atm.getBalance());
+    }
+    TEST(Atm, AtmWithdrawMinus)
+    {
+        Atm atm(1000);
+        atm.withdrawal(-1000);
+        int expected = 1000;
+        EXPECT_EQ(expected, atm.getBalance());
+    }
+    TEST(Atm, AtmWithdrawBalanceMinus)
+    {
+        Atm atm(1000);
+        atm.withdrawal(2000);
+        int expected = 1000;
+        EXPECT_EQ(expected, atm.getBalance());
+    }
+
     // 残高以上の引き出し、振込の関数を新しく作り、残高が移動しているかなど 
+
+    // Aridome
+
+    TEST(Atm, AtmBalanceWithCharacter){
+        Atm atm(9999999999999999999999999999999);
+
+        int expected = 0;
+        EXPECT_EQ(expected, atm.getBalance());
+    }
 }
